39bd2422bd102afd463fe5050b404c56b4dc4398
Shivani <shivanikhandelwal487@gmail.com>
2024-02-21 01:50:37 +0000

    Day8 and Day9 Questions

--- Day9_Q2.py
+++ Day9_Q2.py
@@ -1,3 +1,5 @@
+# Question - https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
+
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
         # Method 1 ->
